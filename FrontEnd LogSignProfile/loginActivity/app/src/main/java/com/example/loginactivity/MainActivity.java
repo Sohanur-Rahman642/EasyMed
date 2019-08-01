16a782573938be69985278a1a07af16d3627554a
unknown <m.tahmeed195715@gmail.com>
2019-08-01 21:51:21 +0600

    Modified Login

--- FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/MainActivity.java
+++ FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -2,33 +2,40 @@ package com.example.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText Email;
-    private EditText Password;
-    private Button Login;
-
+    Button a,b,c,d,e,f,g,h,i,j;
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Email = (EditText)findViewById(R.id.txtEmail);
-        Password = (EditText)findViewById(R.id.txtPassword);
-        Login = (Button)findViewById(R.id.btnLogin);
+
+        f=(Button)findViewById(R.id.bSignIn);
+        f.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
 
 
+            }
 
 
+        });
     }
-
-
-//    private  void validate(String userEmail, String userPassword){
-//        if ((userEmail == "m.tahmeed195715@gmail.com") && (userPassword == "123456789"))
-//            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-//    }
 }
