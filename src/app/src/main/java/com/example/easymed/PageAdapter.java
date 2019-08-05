26ba6d8a217b7e3e66ee913ec67e1825aef2624a
Sohanur-Rahman-642 <rahman.sohanur@northsouth.edu>
2019-08-06 03:33:01 +0600

    Update PageAdapter
    Implemented FragmentPageAdapter interface

---
+++ src/app/src/main/java/com/example/easymed/PageAdapter.java
@@ -0,0 +1,28 @@
+package com.example.easymed;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+    public PageAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+
+}
