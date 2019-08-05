a7017f605d91b5d2d73b7a22a10342e6b8a374e1
Sohanur-Rahman-642 <rahman.sohanur@northsouth.edu>
2019-08-06 03:02:16 +0600

    Added TabLayout
    # Things have been changed
    1-- Created 3 tab items (Store, Brand, Symptoms)
    a-- Store -> medicine selling view
    b-- Brand -> list of medicine company
    c--  Symptoms -> Add symptoms view

--- src/app/src/main/java/com/example/easymed/MainActivity.java
+++ src/app/src/main/java/com/example/easymed/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.easymed;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
