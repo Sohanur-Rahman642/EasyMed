3bd86d57b8467edf1f20870453198c413f835de6
Sohanur-Rahman-642 <rahman.sohanur@northsouth.edu>
2019-08-06 03:43:39 +0600

    Added Constructor on MedicineAdapter class

--- src/app/src/main/java/com/example/easymed/MedicineAdapter.java
+++ src/app/src/main/java/com/example/easymed/MedicineAdapter.java
@@ -1,5 +1,6 @@
 package com.example.easymed;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -9,53 +10,62 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MedicineAdapter extends  RecyclerView.Adapter<MedicineAdapter.MedicineViewHolder> {
-    {
 
+
+    public Context context;
+    List<Medicine> medicineList;
+
+    public MedicineAdapter(Context context, List<Medicine> medicineList) {
+        this.context = context;
+        this.medicineList = medicineList;
     }
+    
 
-    @NonNull
-    @Override
-    public MedicineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        @NonNull
+        @Override
+        public MedicineViewHolder onCreateViewHolder (@NonNull ViewGroup parent,int viewType){
         return null;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull MedicineViewHolder holder, int position) {
+        @Override
+        public void onBindViewHolder (@NonNull MedicineViewHolder holder,int position){
 
     }
 
-    @Override
-    public int getItemCount() {
+        @Override
+        public int getItemCount () {
         return 0;
     }
 
 
-    public class MedicineViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private TextView tvMedcicineName;
-        private TextView tvMediBrandName;
-        private TextView tvMediPower;
-        private ImageView mediImage;
-        private Button quickViewButton;
-        private Button addToCartButton;
+        public class MedicineViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            private TextView tvMedcicineName;
+            private TextView tvMediBrandName;
+            private TextView tvMediPower;
+            private ImageView mediImage;
+            private Button quickViewButton;
+            private Button addToCartButton;
 
-        public MedicineViewHolder(View itemView) {
-            super(itemView);
+            public MedicineViewHolder(View itemView) {
+                super(itemView);
 
-            tvMedcicineName = (TextView) itemView.findViewById(R.id.text_name);
-            tvMediBrandName = (TextView) itemView.findViewById(R.id.text_brand_name);
-            tvMediPower = (TextView) itemView.findViewById(R.id.text_power);
-            mediImage = (ImageView) itemView.findViewById(R.id.store_image);
-            quickViewButton = (Button) itemView.findViewById(R.id.quick_view_btn);
-            addToCartButton = (Button) itemView.findViewById(R.id.add_to_cart_btn);
-            itemView.setOnClickListener(this);
+                tvMedcicineName = (TextView) itemView.findViewById(R.id.text_name);
+                tvMediBrandName = (TextView) itemView.findViewById(R.id.text_brand_name);
+                tvMediPower = (TextView) itemView.findViewById(R.id.text_power);
+                mediImage = (ImageView) itemView.findViewById(R.id.store_image);
+                quickViewButton = (Button) itemView.findViewById(R.id.quick_view_btn);
+                addToCartButton = (Button) itemView.findViewById(R.id.add_to_cart_btn);
+                itemView.setOnClickListener(this);
 
 
-        }
+            }
 
-        @Override
-        public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
+            }
         }
     }
-}
