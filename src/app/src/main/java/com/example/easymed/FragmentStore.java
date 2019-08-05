935a9cde02d6cf8f571e06f2041dbf4304db6bc9
Sohanur-Rahman-642 <rahman.sohanur@northsouth.edu>
2019-08-06 04:03:13 +0600

    Added RecyclerView codes in FragmentStore.java class

--- src/app/src/main/java/com/example/easymed/FragmentStore.java
+++ src/app/src/main/java/com/example/easymed/FragmentStore.java
@@ -8,10 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentStore extends Fragment {
 
-    View view;
+    MedicineAdapter medicineAdapter;
+    List<Medicine> medicineList;
 
 
 
@@ -20,7 +27,89 @@ public class FragmentStore extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.store_fragment,container,false);
-        return view;
+        View rootView = inflater.inflate(R.layout.store_fragment, container, false);
+
+
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.store_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemViewCacheSize(20);
+        recyclerView.setDrawingCacheEnabled(true);
+        recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+
+        medicineList = new ArrayList<>();
+
+
+        medicineList.add(
+                new Medicine(
+                        "Ace +",
+                        "SQUARE",
+                        "500 mg",
+                        R.drawable.aceplus
+
+                )
+        );
+
+        medicineList.add(
+                new Medicine(
+                        "Ace +",
+                        "SQUARE",
+                        "500 mg",
+                        R.drawable.aceplus
+
+                )
+        );
+        medicineList.add(
+                new Medicine(
+                        "Ace +",
+                        "SQUARE",
+                        "500 mg",
+                        R.drawable.aceplus
+
+                )
+        );
+
+
+        medicineList.add(
+                new Medicine(
+                        "Ace +",
+                        "SQUARE",
+                        "500 mg",
+                        R.drawable.aceplus
+
+                )
+        );
+
+        medicineList.add(
+                new Medicine(
+                        "Ace +",
+                        "SQUARE",
+                        "500 mg",
+                        R.drawable.aceplus
+
+                )
+        );
+
+        medicineList.add(
+                new Medicine(
+                        "Ace +",
+                        "SQUARE",
+                        "500 mg",
+                        R.drawable.aceplus
+
+                )
+        );
+
+        MedicineAdapter adapter = new MedicineAdapter(getContext(),medicineList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+
+
+
+
+        return rootView;
     }
 }
