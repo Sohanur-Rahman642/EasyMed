05498440a39135a9a7139c07e3a103baf799b547
Sohanur-Rahman-642 <rahman.sohanur@northsouth.edu>
2019-08-06 03:59:18 +0600

    Added TabLayout.OnTabSelectedListener() on MainActivity.java class

--- src/app/src/main/java/com/example/easymed/MainActivity.java
+++ src/app/src/main/java/com/example/easymed/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.easymed;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
@@ -35,6 +36,47 @@ public class MainActivity extends AppCompatActivity {
 
         pageAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(pageAdapter);
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                Fragment selectedFragment = null;
+                if(tab.getPosition() == 1){
+
+                    selectedFragment = new FragmentStore();
+
+                }
+                if(tab.getPosition() == 2){
+
+                    selectedFragment = new FragmentBrand();
+
+                }
+                if(tab.getPosition() == 3){
+
+                    selectedFragment = new FragmentSuggestion();
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
 
 
 
