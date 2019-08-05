1766b70c2e208e7c6eae82c904e5edeaec275e82
Sohanur-Rahman-642 <rahman.sohanur@northsouth.edu>
2019-08-06 03:52:04 +0600

    Update MainActivity.java class
    Added instances
    set viewpager adapter with pageAdapter

--- src/app/src/main/java/com/example/easymed/MainActivity.java
+++ src/app/src/main/java/com/example/easymed/MainActivity.java
@@ -3,15 +3,39 @@ package com.example.easymed;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private PageAdapter pageAdapter;
+    private TabItem tabStore;
+    private TabItem tabBrand;
+    private TabItem tabSuggestion;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabStore = findViewById(R.id.tab_store);
+        tabBrand = findViewById(R.id.tab_brand);
+        tabSuggestion = findViewById(R.id.tab_suggestion);
+        viewPager = findViewById(R.id.viewpager);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
 
 
 
