a3f653446b15db01b7e647e931d853951ea20847
Muhammad-Abeer <52423250+Muhammad-Abeer@users.noreply.github.com>
2019-08-19 02:58:55 +0600

    loginButton & signUp button referrence has been set

--- src/app/src/main/java/com/example/easymed/LoginActivity.java
+++ src/app/src/main/java/com/example/easymed/LoginActivity.java
@@ -2,12 +2,20 @@ package com.example.easymed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton,signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+
     }
 }
