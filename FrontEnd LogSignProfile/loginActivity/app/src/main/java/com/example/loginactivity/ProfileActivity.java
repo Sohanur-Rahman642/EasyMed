f9ff5ba6e13497dfe09059ecd2ca9f50cb15e4d8
unknown <m.tahmeed195715@gmail.com>
2019-08-18 23:58:43 +0600

    Edit and update

--- FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/ProfileActivity.java
+++ FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/ProfileActivity.java
@@ -2,13 +2,33 @@ package com.example.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    Button g;
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+
+        g=(Button)findViewById(R.id.bBack1);
+        g.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(ProfileActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/SecondActivity.java
+++ FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/SecondActivity.java
@@ -2,13 +2,45 @@ package com.example.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Button f;
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_profile);
+
+        f=(Button)findViewById(R.id.bProfile);
+        f.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+
+                Intent intent =new Intent(SecondActivity.this,ProfileActivity.class);
+                startActivity(intent);
+
+
+            }
+
+
+
+
+        });
+
+
+
+
     }
 }
