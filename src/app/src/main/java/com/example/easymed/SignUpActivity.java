e995beb965f5973d5fe06d2ec46ecfa42baae749
Muhammad-Abeer <52423250+Muhammad-Abeer@users.noreply.github.com>
2019-08-19 03:19:09 +0600

    reference added for buttons in signupActivity

--- src/app/src/main/java/com/example/easymed/SignUpActivity.java
+++ src/app/src/main/java/com/example/easymed/SignUpActivity.java
@@ -2,12 +2,20 @@ package com.example.easymed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Button caButton,alreadyCaButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        caButton = (Button) findViewById(R.id.caButton);
+        alreadyCaButton = (Button) findViewById(R.id.alreadyCa_button);
+
     }
 }
