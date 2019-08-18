7bf1fb0167d36ac679740091049bddbb0f7fde82
Muhammad-Abeer <52423250+Muhammad-Abeer@users.noreply.github.com>
2019-08-19 03:21:15 +0600

    onclick listener added for already have acc button

--- src/app/src/main/java/com/example/easymed/SignUpActivity.java
+++ src/app/src/main/java/com/example/easymed/SignUpActivity.java
@@ -29,5 +29,13 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+        alreadyCaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
