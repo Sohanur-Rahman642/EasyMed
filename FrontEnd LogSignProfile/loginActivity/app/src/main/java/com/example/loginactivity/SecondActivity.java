d11cf848a559232402dfbc049c553250f573c13a
unknown <m.tahmeed195715@gmail.com>
2019-08-19 00:28:06 +0600

    Editing profile and java code

--- FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/SecondActivity.java
+++ FrontEnd LogSignProfile/loginActivity/app/src/main/java/com/example/loginactivity/SecondActivity.java
@@ -7,18 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class SecondActivity extends AppCompatActivity {
 
-    Button f;
+    Button i;
     Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_second);
 
-        f=(Button)findViewById(R.id.bProfile);
-        f.setOnClickListener(new View.OnClickListener() {
+        ImageView i = (ImageView) findViewById(R.id.bProfile);
+
+        i.setOnClickListener(new View.OnClickListener() {
 
             /**
              * Called when a view has been clicked.
