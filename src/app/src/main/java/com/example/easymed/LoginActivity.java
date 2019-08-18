6b19039f2f96ab1c7da8720b60e76bf1a3987018
Muhammad-Abeer <52423250+Muhammad-Abeer@users.noreply.github.com>
2019-08-19 03:00:38 +0600

    onclick listener added for loginButton

--- src/app/src/main/java/com/example/easymed/LoginActivity.java
+++ src/app/src/main/java/com/example/easymed/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.easymed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,6 +17,16 @@ public class LoginActivity extends AppCompatActivity {
 
         loginButton = (Button) findViewById(R.id.loginButton);
         signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
