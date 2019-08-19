4e84b95199f571cd259422114e4bb9b12298ad44
mehedirahman01 <mehedi.rahman01@northsouth.edu>
2019-08-19 23:22:30 +0600

    added java code for book an apointment button click

--- src/app/src/main/java/com/example/easymed/ExpertCounselling.java
+++ src/app/src/main/java/com/example/easymed/ExpertCounselling.java
@@ -1,15 +1,36 @@
 package com.example.easymed;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExpertCounselling extends AppCompatActivity {
+    Button book_an_appointment_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expert_counselling);
+
+
+        book_an_appointment_btn = (Button) findViewById(R.id.baap_btn);
+
+
+        book_an_appointment_btn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+
+
+
+                Intent intent = new Intent(ExpertCounselling.this,SubmitDetailsForAppointment.class);
+                startActivity(intent);
+            }
+        });
     }
 }
