016c48a24e80deb3c895d69ec51bf14a16a9c0e6
mehedirahman01 <mehedi.rahman01@northsouth.edu>
2019-08-19 22:41:31 +0600

    circle imageview added into expert counselling

---
+++ src/app/src/main/java/com/example/easymed/ExpertCounselling.java
@@ -0,0 +1,15 @@
+package com.example.easymed;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExpertCounselling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expert_counselling);
+    }
+}
