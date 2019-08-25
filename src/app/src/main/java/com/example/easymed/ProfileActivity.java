d4b66840d34b2ea1d55643a36140773c7b39545e
unknown <m.tahmeed195715@gmail.com>
2019-08-26 01:25:06 +0600

    Update Profile Activity -Backend

--- src/app/src/main/java/com/example/easymed/ProfileActivity.java
+++ src/app/src/main/java/com/example/easymed/ProfileActivity.java
@@ -5,17 +5,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
     Button updateButton, signOut;
+    private TextView viewName, viewEmail, viewPhone, viewAddress;
+
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));;
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        viewName = (TextView) findViewById(R.id.pr_user_name);
+        viewName.setText("Welcome "+user.getDisplayName());
+
+        viewEmail = (TextView) findViewById(R.id.pr_email_id);
+        viewPhone = (TextView) findViewById(R.id.pr_contact_number);
+        viewAddress = (TextView) findViewById(R.id.pr_ad_address);
+
 
         updateButton = (Button) findViewById(R.id.update_btn);
         signOut = (Button) findViewById(R.id.sign_out_btn);
