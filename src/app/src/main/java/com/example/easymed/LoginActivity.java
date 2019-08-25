8bfd8330c0bddb70be581e5b74f6d1bc77476f68
unknown <m.tahmeed195715@gmail.com>
2019-08-25 16:27:49 +0600

    Backend Login

--- src/app/src/main/java/com/example/easymed/LoginActivity.java
+++ src/app/src/main/java/com/example/easymed/LoginActivity.java
@@ -1,31 +1,64 @@
 package com.example.easymed;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button loginButton,
     signUpButton;
 
+    private EditText userEmail2;
+    private EditText userPassword2;
+
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null){
+            //profile activity/ main activity
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+
+        userEmail2 = (EditText) findViewById(R.id.email_input_field);
+        userPassword2 = (EditText) findViewById(R.id.password_input_field);
+
         loginButton = (Button) findViewById(R.id.loginButton);
         signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        progressBar = new ProgressBar(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+//                startActivity(intent);
+
+                userLogin();
             }
         });
 
@@ -41,5 +74,39 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
+
+
     }
+
+    private void userLogin(){
+        String email2 = userEmail2.getText().toString().trim();
+        String password2 = userPassword2.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email2)){
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password2)){
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        firebaseAuth.signInWithEmailAndPassword(email2, password2)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+
+                        if(task.isSuccessful()){
+                            progressBar.setVisibility(View.GONE);
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                    }
+                });
+
+    }
+
+
 }
--- src/app/src/main/java/com/example/easymed/SignUpActivity.java
+++ src/app/src/main/java/com/example/easymed/SignUpActivity.java
@@ -1,6 +1,18 @@
 package com.example.easymed;
 
 
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import android.content.Intent;
 
 import android.os.Bundle;
