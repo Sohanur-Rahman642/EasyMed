5513256ca0566d4b2f6e1d6a31a17b317895cfe1
unknown <m.tahmeed195715@gmail.com>
2019-08-26 12:36:04 +0600

    Login backend updated

--- src/app/src/main/java/com/example/easymed/LoginActivity.java
+++ src/app/src/main/java/com/example/easymed/LoginActivity.java
@@ -1,112 +1,148 @@
 package com.example.easymed;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
-
 import android.os.Bundle;
+
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public  class LoginActivity extends AppCompatActivity {
 
-    Button loginButton,
-    signUpButton;
+    private Button loginButton;
+    private EditText loginEmail, loginPassword;
+    private Button loginSignup;
+    private ProgressDialog loadingBar;
+    private FirebaseAuth mAuth;
 
-    private EditText userEmail2;
-    private EditText userPassword2;
+    private static final int RC_SIGN_IN = 1;
 
-    private ProgressBar progressBar;
+    private static final String TAG = "LoginUser";
 
-    private FirebaseAuth firebaseAuth;
 
+
+    public static String loginEmail1, loginPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser() != null){
-            //profile activity/ main activity
-            finish();
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        }
+        loginSignup = findViewById(R.id.signUpButton);
+        loginEmail = findViewById(R.id.email_edit_text);
+        loginPassword = findViewById(R.id.password_edit_text);
+        loginButton = findViewById(R.id.loginButton);
 
-        userEmail2 = (EditText) findViewById(R.id.email_input_field);
-        userPassword2 = (EditText) findViewById(R.id.password_input_field);
+        //login_forgot_pass=findViewById(R.id.login_forgot_pass);
 
-        loginButton = (Button) findViewById(R.id.loginButton);
-        signUpButton = (Button) findViewById(R.id.signUpButton);
+        loadingBar = new ProgressDialog(this);
 
-        progressBar = new ProgressBar(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        loginEmail1 = loginEmail.getText().toString().trim();
+        loginPass = loginPassword.getText().toString().trim();
+
+
+        loginSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SendUserToSignUpActivity();
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-//                startActivity(intent);
 
-                userLogin();
+                AllowingUserToLogin();
             }
         });
-
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-
-
 
     }
+        @Override
+        protected void onStart(){
+            super.onStart();
 
-    private void userLogin(){
-        String email2 = userEmail2.getText().toString().trim();
-        String password2 = userPassword2.getText().toString().trim();
+            FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        if(TextUtils.isEmpty(email2)){
-            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
-            return;
+            if (currentUser != null) {
+
+                SendUserToHome();
+            }
         }
-        if (TextUtils.isEmpty(password2)){
-            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
-        }
 
-        progressBar.setVisibility(View.VISIBLE);
+        private void AllowingUserToLogin () {
 
-        firebaseAuth.signInWithEmailAndPassword(email2, password2)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            String email = loginEmail.getText().toString();
+            String password = loginPassword.getText().toString();
+
+            if (TextUtils.isEmpty(email)) {
+
+                Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(password)) {
+
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            } else {
+
+                loadingBar.setTitle("Login");
+                loadingBar.setMessage("Please wait until we are logging into your account...");
+                loadingBar.setCanceledOnTouchOutside(true);
+                loadingBar.show();
+
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
+                        if (task.isSuccessful()) {
 
-                        if(task.isSuccessful()){
-                            progressBar.setVisibility(View.GONE);
-                            finish();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            SendUserToHome();
+
+                            loadingBar.dismiss();
+                        } else {
+
+                            String message = task.getException().getMessage();
+                            Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
                         }
                     }
                 });
+            }
+        }
 
+
+        private void SendUserToHome () {
+
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+            finish();
+        }
+
+
+        private void SendUserToSignUpActivity () {
+
+            Intent registerIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(registerIntent);
+        }
     }
 
 
-}
